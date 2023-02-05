43c8bb30ee5be8bd2ad90cde826e85b314711b79
leharris3 <levlevi@email.unc.edu>
2023-02-05 13:01:24 -0500

    -j fixed

--- cli.js
+++ cli.js
@@ -28,13 +28,20 @@ if ((argv.n && argv.s) || (argv.e && argv.w)) {
 async function getWeather() { 
   let response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=weathercode,temperature_2m_max,precipitation_hours,windspeed_10m_max,winddirection_10m_dominant&timezone=' + timezone);
   let data = await response.json()
-  // console.log(data)
+  
+  if (argv.j) {
+    console.log(data)
+    process.exit(0)
+  }
+
   if (data.daily.precipitation_hours[days] > 0) {
     console.log("You might need your galoshes " + day)
   }
   else {
     console.log("You probably won't need your galoshes " + day);
+  
   }
+  
   return data;
 }
 
@@ -46,10 +53,6 @@ if (argv.h){
     -z            Time zone: uses tz.guess() from moment-timezone by default.
     -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
     - j            Echo pretty JSON from open - meteo API and exit.in / bash`)
-  process.exit(0)
-}
-else if (argv.j) {
-  getWeather().then(data => console.log(data))
   process.exit(0)
 }
 
