22af7af43f00587da22681fdc86bff61ac8008f0
leharris3 <levlevi@email.unc.edu>
2023-02-05 12:46:02 -0500

    basic functionality

--- cli.js
+++ cli.js
@@ -5,30 +5,31 @@ var argv = require('minimist')(process.argv.slice(2));
 
 const latitude = argv.n || argv.s
 const longitude = argv.w || argv.e
-const date = argv.d
 const timezone = argv.z || "America/New_York"
 
-var weather = []
-async function getWeather() { 
-    let response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=weathercode,temperature_2m_max,precipitation_hours,windspeed_10m_max,winddirection_10m_dominant&timezone=' + timezone);
-    let data = await response.json()
-    const jsonString = JSON.parse(JSON.stringify(data))
-    for (var i in jsonString)
-        weather.push(jsonString[i])
-    // console.log(data.daily.precipitation_hours[1])
-    return data;
-}
+var day = ""
+const days = argv.d
 
-const days = argv.d 
 if (days == 0) {
-  console.log("today.")
+  day = "today."
 } else if (days > 1) {
-  console.log("in " + days + " days.")
+  day = "in " + days + " days."
 } else {
-  console.log("tomorrow.")
+  day = "tomorrow."
 }
 
-getWeather()
+async function getWeather() { 
+  let response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=weathercode,temperature_2m_max,precipitation_hours,windspeed_10m_max,winddirection_10m_dominant&timezone=' + timezone);
+  let data = await response.json()
+  // console.log(data)
+  if (data.daily.precipitation_hours[days] > 0) {
+    console.log("You might need your galoshes " + day)
+  }
+  else {
+    console.log("You probably won't need your galoshes " + day);
+  }
+  return data;
+}
 
 
 if (argv.h){
@@ -39,7 +40,11 @@ if (argv.h){
     -z            Time zone: uses tz.guess() from moment-timezone by default.
     -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
     - j            Echo pretty JSON from open - meteo API and exit.in / bash`)
+  process.exit(0)
 }
 else if (argv.j) {
-    getWeather().then(data => console.log(data));
+  getWeather().then(data => console.log(data))
+  process.exit(0)
 }
+
+getWeather() // Main Function Driver
